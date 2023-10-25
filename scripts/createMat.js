6f3affd2b05c121260300268261924a4231906bd
nikoscham <nikoscham@gmail.com>
2023-10-25 18:16:38 +0300

    Commenting and optimizing

--- scripts/createMat.js
+++ scripts/createMat.js
@@ -11,6 +11,7 @@
 import { createCoord2D, nodNum2D } from './createCoord.js';
 import { basisFunQuad2D } from './basisFun.js';
 
+// This function creates the matrix and the residual vector for the Finite Element Method in two dimensions
 export function createLaplaceMat2D(nex, ney, xlast, ylast) {
 
   // Generate x-y coordinates using createCoord2D function
@@ -22,12 +23,12 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
   // Initialize variables for matrix assembly
   const ne = nex * ney; // Total number of elements
   const np = nnx * nny; // Total number of nodes
-  let ntop = []; // Neumann boundary condition flag (elements at the top side of the domain)
-  let nbottom = []; // Neumann boundary condition flag (elements at the bottom side of the domain)
-  let nleft = []; // Neumann boundary condition flag (elements at the left side of the domain)
-  let nright = []; // Neumann boundary condition flag (elements at the right side of the domain)
-  let ncod = []; // Dirichlet boundary condition flag
-  let bc = []; // Dirichlet boundary condition value
+  let ntop = new Array(ne).fill(0); // Neumann boundary condition flag (elements at the top side of the domain)
+  let nbottom = new Array(ne).fill(0); // Neumann boundary condition flag (elements at the bottom side of the domain)
+  let nleft = new Array(ne).fill(0); // Neumann boundary condition flag (elements at the left side of the domain)
+  let nright = new Array(ne).fill(0); // Neumann boundary condition flag (elements at the right side of the domain)
+  let ncod = new Array(np).fill(0); // Dirichlet boundary condition flag
+  let bc = new Array(np).fill(0); // Dirichlet boundary condition value
   let ngl = []; // Local nodal numbering
   let gp = []; // Gauss points
   let wgp = []; // Gauss weights
@@ -35,15 +36,13 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
   let phy = []; // The y-derivative of the basis function
   let res = []; // Galerkin residuals
   let jac = []; // Jacobian matrix
-  let x; // x-coordinate
-  let y; // y-coordinate
+  let x; // x-coordinate (physical coordinates)
+  let y; // y-coordinate (physical coordinates)
   let x1; // ksi-derivative of x
-  let x2; // eta-derivative of x
+  let x2; // eta-derivative of x (ksi and eta are natural coordinates that vary within a reference element)
   let y1; // ksi-derivative of y
   let y2; // eta-derivative of y
   let dett; // The jacobian of the isoparametric mapping
-  let m1;
-  let n1;
 
   // Initialize jac and res arrays
   for (let i = 0; i < np; i++) {
@@ -54,45 +53,35 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
     }
   }
 
-  // Neumann boundary conditions
-  for (let i = 0; i < ne; i++) {
-    ntop[i] = 0;
-    nbottom[i] = 0;
-    nleft[i] = 0;
-    nright[i] = 0;
+  // Impose Neumann boundary conditions
+  for (let i = 0; i < ne - ney; i += ney) { // Define ntop for elements along y=yfirst (bottom side of the domain)
+  //  nbottom[i] = 1;
+  }
+  for (let i = 0; i < ney; i++) { // Define ntop for elements along x=xfirst (left side of the domain)
+  //  nleft[i] = 1;
+  }
+  for (let i = ney - 1; i < ne; i += ney) { // Define ntop for elements along y=ylast (top side of the domain)
+    ntop[i] = 1;
+  }
+  for (let i = ne - ney; i < ne; i++) { // Define ntop for elements along x=xlast (right side of the domain)
+  //  nright[i] = 1;
   }
 
-  // Define ntop for elements along y=yfirst
-  for (let i = 0; i < ne - ney; i += ney) {
-  //  ntop[i] = 1;
-  //  nbottom[i] = 1;
-  //  nleft[i] = 1;
-  //  nright[i] = 1;
-  } 
-
-  // Define ntop for elements along x=xfirst
-  for (let i = 0; i < ney; i++) {
-  //  ntop[i] = 1;
-  //  nbottom[i] = 1;
-  //  nleft[i] = 1;
-  //  nright[i] = 1;
-  } 
-
-  // Define ntop for elements along y=ylast
-  for (let i = ney - 1; i < ne; i += ney) {
-    ntop[i] = 1;
-  //  nbottom[i] = 1;
-  //  nleft[i] = 1;
-  //  nright[i] = 1;
-  } 
-  
-  // Define ntop for elements along x=xlast
-  for (let i = ne - ney; i < ne; i++) {
-  //  ntop[i] = 1;
-  //  nbottom[i] = 1;
-  //  nleft[i] = 1;
-  //  nright[i] = 1;
-  } 
+  // // Impose Neumann boundary conditions (alternative -easier to read- method)
+  // for (let i = 0; i < ne; i++) {
+  //   if (aypt[nop[i][8]] == ylast) { // Check if element is at the top side of the domain (y = ylast)
+  //     ntop[i] = +1;
+  //   }
+  //   if (aypt[nop[i][0]] == yfirst) { // Check if element is at the bottom side of the domain (y = yfirst)
+  //     nbottom[i] = +1;
+  //   }
+  //   if (axpt[nop[i][0]] == xfirst) { // Check if element is at the left side of the domain (x  = xfirst)
+  //     nleft[i] = +1;
+  //   }
+  //   if (axpt[nop[i][6]] == xlast) { // Check if element is at the right side of the domain (x =xlast)
+  //     nright[i] = +1;
+  //   }
+  //  }
 
   // Matrix assembly
   for (let i = 0; i < ne; i++) {
@@ -115,12 +104,7 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
 
         // Initialise variables for isoparametric mapping
         let { ph, phic, phie } = basisFunQuad2D(gp[j], gp[k]);
-        x = 0;
-        y = 0;
-        x1 = 0;
-        x2 = 0;
-        y1 = 0;
-        y2 = 0;
+        x = y = x1 = x2 = y1 = y2 = dett = 0;
 
         // Isoparametric mapping
         for (let n = 0; n < 9; n++) {
@@ -141,11 +125,11 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
 
         // Computation of Galerkin's residuals and Jacobian matrix
         for (let m = 0; m < 9; m++) {
-          m1 = ngl[m];
+          let m1 = ngl[m];
           res[m1] += wgp[j] * wgp[k] * dett * ph[m];
 
           for (let n = 0; n < 9; n++) {
-            n1 = ngl[n];
+            let n1 = ngl[n];
             jac[m1][n1] += -wgp[j] * wgp[k] * dett * (phx[m] * phx[n] + phy[m] * phy[n]);
           }
         }
@@ -153,15 +137,16 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
     }
 
     // Check for elements to impose Neumann boundary conditions
-    //
-    // Representation of the nodes in the case of quadratic rectangular elements
-    //  
-    //  2__5__8
-    //  |     |
-    //  1  4  7
-    //  |__ __|
-    //  0  3  6
-    //
+    /*
+    Representation of the nodes in the case of quadratic rectangular elements
+    
+      2__5__8
+      |     |
+      1  4  7
+      |__ __|
+      0  3  6
+
+    */
     if (ntop[i] == 1 || nbottom[i] == 1 || nleft[i] == 1 || nright[i] == 1) {
       for (let n = 0; n < 3; n++) {
         let gp1, gp2, firstNode, finalNode, nodeIncr;
@@ -195,57 +180,65 @@ export function createLaplaceMat2D(nex, ney, xlast, ylast) {
           finalNode = 9;
           nodeIncr = 1;
         }
+        // Evaluate the basis functions and their derivatives at the Gauss point
         let { ph, phic, phie } = basisFunQuad2D(gp1, gp2);
-        x = 0;
-        x1 = 0;
+        x = x1 = 0;
         for (let k = 0; k < 9; k++) {
-          x += axpt[ngl[k]] * ph[k];
-          x1 += axpt[ngl[k]] * phic[k];
+          x += axpt[ngl[k]] * ph[k]; // Interpolate the x-coordinate at the Gauss point
+          x1 += axpt[ngl[k]] * phic[k]; // Interpolate the ksi-derivative of x at the Gauss point
         }
         for (let m1 = firstNode; m1 < finalNode; m1 += nodeIncr) {
-          res[m1] += -wgp[n] * x1 * ph[m1];
+          res[m1] += -wgp[n] * x1 * ph[m1]; // Add the Neumann boundary term to the residual vector
         }
       }
     }
   }
 
   // Impose Dirichlet boundary conditions
-  for (let i = 0; i < np; i++) {
-    ncod[i] = 0;
-    bc[i] = 0;
- }
-
-  // Define ncod and bc for nodes on y=yfirst
-  for (let i = 0; i < np - nny + 1; i += nny) {
+  for (let i = 0; i < np - nny + 1; i += nny) { // Define ncod and bc for nodes on y=yfirst (bottom side of the domain)
     ncod[i] = 1;
     bc[i] = 1;
   }
-
-  // Define ncod and bc for nodes on x=xfirst
-  for (let i = 0; i < nny; i++) {
+  for (let i = 0; i < nny; i++) { // Define ncod and bc for nodes on x=xfirst (left side of the domain)
     ncod[i] = 1;
     bc[i] = 1;
   }
-
-  // Define ncod and bc for nodes on y=ylast
-  //for (let i = nny - 1; i < np; i += nny) {
+  //for (let i = nny - 1; i < np; i += nny) { // Define ncod and bc for nodes on y=ylast (top side of the domain)
   //  ncod[i] = 1;
   //  bc[i] = 1;
   //} 
-
-  // Define ncod and bc for nodes on x=xlast
-  for (let i = np - nny; i < np; i++) {
+  for (let i = np - nny; i < np; i++) { // Define ncod and bc for nodes on x=xlast (right side of the domain)
     ncod[i] = 1;
     bc[i] = 1;
   }
+
+  // // Impose Dirichlet boundary conditions (alternative -easier to read- method)
+  // for (let i = 0; i < np; i++) {
+  //   if (aypt[i] == yfirst) { // Check if node is at the bottom side of the domain
+  //     ncod[i] = 1;
+  //     bc[i] = 1; // Assign a value or a function to the node's unknown field
+  //   }
+  //   if (aypt[i] == ylast) { // Check if node is at the top side of the domain
+  //     ncod[i] = 1;
+  //     bc[i] = 1; // Assign a value or a function to the node's unknown field
+  //   }
+  //   if (axpt[i] == xfirst) { // Check if node is at the left side of the domain
+  //     ncod[i] = 1;
+  //     bc[i] = 1; // Assign a value or a function to the node's unknown field
+  //   }
+  //   if (axpt[i] == xlast) { // Check if node is at the right side of the domain
+  //     ncod[i] = 1;
+  //     bc[i] = 1; // Assign a value or a function to the node's unknown field
+  //   }
+  // }
 
   // Impose Dirichlet boundary conditions
   for (let i = 0; i < np; i++) {
     if (ncod[i] == 1) {
-      res[i] = bc[i];
+      res[i] = bc[i]; // Set the residual vector to the Dirichlet value
       for (let j = 0; j < np; j++) {
-        jac[i][j] = 0;
-        jac[i][i] = 1;
+        jac[i][j] = 0; // Set the Jacobian matrix to zero
+        jac[i][i] = 1; // Set the diagonal entry to one
       }
     }
   }
