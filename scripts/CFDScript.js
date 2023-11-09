b32cf5043800ed1d59a1118f262f5cdf3c0ea4fc
nikoscham <nikoscham@gmail.com>
2023-11-09 18:58:12 +0200

    Website update

--- scripts/CFDScript.js
+++ scripts/CFDScript.js
@@ -11,28 +11,7 @@
 import { createLaplaceMat2D } from './createMat.js';
 import { gauss } from './auxFun.js';
 
-export function CFDScript() {
-
-  // Definitions
-  let u = []; // Initialize x as an empty array
-  const nex = 5; // Number of elements in x-direction
-  const ney = 5; // Number of elements in y-direction
-  const xlast = 1; // Last x-coordinate
-  const ylast = 1; // Last y-coordinate
-  const boundaryConditions = {
-    neumannTop: true,
-    neumannBottom: false,
-    neumannLeft: false,
-    neumannRight: false,
-    dirichletTop: false,
-    dirichletBottom: true,
-    dirichletLeft: true,
-    dirichletRight: true,
-    dirichletValueTop: 0.0,
-    dirichletValueBottom: 1.0,
-    dirichletValueLeft: 1.0,
-    dirichletValueRight: 1.0,
-  };
+export function CFDScript(nex, ney, xlast, ylast, boundaryConditions) {
   
   // Assembly matrices
   let { jac, res, nnx, nny, axpt, aypt } = createLaplaceMat2D(nex, ney, xlast, ylast, boundaryConditions); // Call createLaplaceMat to assemble the matrices
